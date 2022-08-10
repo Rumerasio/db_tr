e762fd09f4bddebaaf2be1003a8afa756036ee10
Rumerasio <joseph0417@naver.com>
2022-08-10 17:34:02 +0900

    insert into 추가 시작

--- mysql/sql/Simsim.sql
+++ mysql/sql/Simsim.sql
@@ -19,6 +19,33 @@ FROM jsMember a
 WHERE a.id = "escaida15" AND a.password ="fhwpvktmxk" -- ""입력받은 값 
 ;
 
+-- 회원가입 입력
+INSERT INTO jsMember(
+	nickname
+    ,dob
+    ,gender			-- 실제 입력은 숫자입력이 아닌 select 선택
+    ,phoneAgency	-- 실제 입력은 숫자입력이 아닌 select 선택
+    ,phoneNum
+	,emailInsert	-- 실제 이메일 id(앞부분) 입력 받는 것
+    ,emailDomain	-- 실제 입력은 숫자입력이 아닌 select선택
+    ,id
+    ,password
+    ,datetime		-- 사용자 입력이 아닌 누른 순간의 기록
+)
+VALUES (
+	"제미니"
+    ,"1996-03-09"
+    ,101
+    ,202
+    ,"01039395000"
+    ,"woaalsl0309"
+    ,301
+    ,"woaalsdl0309"
+    ,"1q2w3e4r!"
+    ,"2020-06-07"
+)
+;
+
 -- 메인 페이지.상단배너
 SELECT
 	a.survey,
@@ -60,6 +87,24 @@ WHERE 1=1
 AND a.jsSurveyName_seq = 5 -- 별자리 테스트(5) 코멘트 불러오기
 ;
 
+-- 테스트 메인.댓글 입력
+INSERT INTO jsSurveyComment (
+	jsMember_seq
+    ,jsSurveyName_seq
+	,nickname
+    ,commentContent
+    ,datetime
+)
+VALUES (
+	1 -- 회원 id seq
+    ,5 -- 등록하는 테스트 id seq
+    ,"ㅁㄴㅇㄹ" -- nickname, 비회원의 경우 자유로이 설정
+    ,"대체 이게 별자리랑 무슨 상관임 ㅋㅋ"
+    ,"2022-07-05 11:26:37" -- 직접 입력하는 것이 아닌 등록을 누르는 순간 시간 기록
+)
+;
+
+-- 테스트 시작시 생성되는 jsSurveyRecord
 
 -- 테스트와 그 문제, 그리고 그 선택지
 SELECT
@@ -73,6 +118,9 @@ FROM jsSurveyName a
 inner join jsSurveyQuestion b on b.jsSurveyName_seq = a.seq
 inner join jsQuestionChoice c on b.seq = c.jsSurveyQuestion_seq
 ;
+
+-- 제출될 데이터 입력 쿼리
+INSERT INTO 
 
 -- 제출 (hidden)
 SELECT
@@ -150,7 +198,7 @@ inner join jsMember b on b.seq = a.jsSurveyName_seq AND a.jsMember_seq =2
 inner join jsSurveyName c on c.seq = a.jsSurveyName_seq
 ;
 
--- (관리자) 테스트 기록 세부내역
+-- 
 SELECT 
 	d.seq, -- 테스트 기록 seq 
     f.nickname,
